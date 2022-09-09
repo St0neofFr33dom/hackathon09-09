5772db38ff546923df371346340136000b86210e
kendallgmason <93265601+kendallgmason@users.noreply.github.com>
2022-09-09 10:41:19 +0100

    useEffect added and fetch request to the App js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import Detailedrecipe from './components/Detailedrecipe';
 import Mealcard from './components/Mealcard';
 import Navbar from './components/Navbar';
@@ -6,6 +7,12 @@ import Search from './components/Search';
 
 
 function App() {
+ const [recipes, setRecipes] = useState([])
+
+  fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=01e39cb5dd804a48aa3f6d9340db1848`)
+       .then((response) => response.json())
+       .then((data) => setRecipes(data));
+
   return (
     <div className="App">
      <Navbar />
