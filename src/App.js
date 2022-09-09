1f0df1eaa45117d95cae764a66ba0a91c92f8641
St0neofFr33dom <christophefreeston@googlemail.com>
2022-09-09 11:04:17 +0100

    Mapping the fetch data into recipes cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Detailedrecipe from './components/Detailedrecipe';
 import Mealcard from './components/Mealcard';
 import Navbar from './components/Navbar';
@@ -7,19 +7,32 @@ import Search from './components/Search';
 
 
 function App() {
- const [recipes, setRecipes] = useState([])
+ const [recipes, setRecipes] = useState()
 
-  fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=01e39cb5dd804a48aa3f6d9340db1848`)
-       .then((response) => response.json())
-       .then((data) => setRecipes(data));
-
+useEffect(() =>{
+  function getData(){
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_APIKEY}&cuisine=italian`)
+    .then((response) => response.json())
+    .then((data) => setRecipes(data));
+  }
+  getData()
+},[])
+  
+if(!recipes){
+  return <h1>Loading...</h1>
+}
   return (
     <div className="App">
      <Navbar />
      <Detailedrecipe />
      <Search />
-     <Mealcard name="Spaghetti Bolognase" url="https://static.fanpage.it/wp-content/uploads/sites/22/2021/06/spaghetti-bolognese.jpg" alt="Spaghetti Bolgonase"/>
+     <button onClick={(()=>{console.log(recipes)})}>Get Data</button>
+     <div>
+{recipes.results.map(recipe=>{return <Mealcard key={recipe.id} name={recipe.title} url={recipe.image} alt={recipe.title}/>})
+}
+     </div>
 
+     
     </div>
   );
 }
